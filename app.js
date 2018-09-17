30f756bf2d1c7b9de58ea6f9cc38865e5252c973
whwz <wachowicz@vp.pl>
2018-09-17 16:25:10 +0200

    add mlab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const commentRoutes     = require("./routes/comments"),
       indexRoutes       = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://whwz:mongopass1@ds259742.mlab.com:59742/yelpcamp", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
